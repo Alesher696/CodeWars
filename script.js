6224fb65c518f01bca224198a2e086c81ed6d0cf
Aleksandr Sherstnev <alesher696@gmail.com>
2023-01-03 20:02:11 +0300

    +1

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-console.log('hello');
+// console.log('hello');
+//
+//Среднее значение и сравнение со средним числом
+
+// function getGrade(s1, s2, s3) {
+//     let score = (s1 + s2 + s3) / 3;
+//     return (score >= 90 && score <= 100) ? "A" : (score >= 80 && score < 90) ? "B" : (score >= 70 && score < 80) ? "C" :
+//         (score >= 60 && score < 70) ? 'D' : (score >= 0 && score < 60) ? 'F' : 'Empty'
+// }
+//
+// console.log(getGrade(50, 50, 50));
+
